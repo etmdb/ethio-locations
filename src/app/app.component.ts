b15ff22ca2f2b6452c3ee55ca56e6af085b590ce
Mahlet <mahletgirma25@gmail.com>
2018-04-02 11:37:57 +0300

    marker and databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 import * as L from 'leaflet';
@@ -9,25 +9,48 @@ import * as L from 'leaflet';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ethio Locations Game';
+	title = 'Ethio Locations Game';
+	lat = null;
+	lang = null;
+	map_layers: L.Layer[] = [L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+	{ 
+		maxZoom: 18, 
+		attribution: '.' 
+	})];
   options = {
-	layers: [
-		L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
-			{ 
-				maxZoom: 18, 
-				attribution: '...' 
-			})
-			],
+	layers: this.map_layers,
 		zoom: 12,
 		center: L.latLng(8.879966, 38.726909)
-};
+	}
+		constructor(private zone: NgZone) {
 
-onMapReady(map: L.Map) {
-    map.on('click', 
-    	<LeafletMouseEvent>(e) => 
-    	{ 
-    		console.log(e.latlng); 
-        this.title = e.latlng;
-    	});
-  }
+		}
+		onMapReady(map: L.Map) {
+			map.on('click', 
+				<LeafletMouseEvent>(e) => 
+				{ 
+					
+					let c = L.marker([e.latlng.lat, e.latlng.lng], {
+						icon: L.icon({
+							 iconSize: [ 25, 41 ],
+							 iconAnchor: [ 13, 41 ],
+							
+							 iconUrl: 'assets/download.png',
+							 //shadowUrl: 'assets/download.png'		
+					})
+					
+					
+						
+				});
+				this.zone.run(() => {
+					this.lat = e.latlng.lat;
+					this.lang = e.latlng.lng;
+				});
+				c.addTo(map);
+				});
+	
+				
+				
+		}
+
 }
