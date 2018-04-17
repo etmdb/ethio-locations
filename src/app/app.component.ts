bdb466c1db23697a3eaa37c68763996a26965fb3
Mahlet <mahletgirma25@gmail.com>
2018-04-17 13:24:43 +0300

    pics modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,9 @@ import { GeoCoordinate } from "./geo-coordinate";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+	zone: any;
+	geoDistanceService: any;
+	items: Array<any> = []
 	title = 'Ethio Locations Game';
 	lat = null;
 	lang = null;
@@ -25,50 +28,59 @@ export class AppComponent {
 		zoom: 12,
 		center: L.latLng(8.879966, 38.726909)
 	}
-		constructor(private zone: NgZone, private geoDistanceService : GeoDistanceService) {
-
+		constructor() {
+			this.items = [
+      { name: 'assets/images/' },
+      { name: 'assets/images/1.jpg' },
+      { name: 'assets/images/2.jpg' },
+			{ name: 'assets/images/3.jpg' },
+			{ name: 'assets/images/4.jpg' },
+			{ name: 'assets/images/5.jpg' },
+			{ name: 'assets/images/6.jpg' },
+			{ name: 'assets/images/7.jpg' },
+			{ name: 'assets/images/8.jpg' },
+			{ name: 'assets/images/1.jpg' },
+			{ name: 'assets/images/2.jpg' },
+			{ name: 'assets/images/3.jpg' },
+    ]
 		}
-		onMapReady(map: L.Map) {
-			map.on('click', 
-				<LeafletMouseEvent>(e) => 
-				{ 
-					
-					let c = L.marker([e.latlng.lat, e.latlng.lng], {
-						icon: L.icon({
-							 iconSize: [ 25, 41 ],
-							 iconAnchor: [ 13, 41 ],
-							
-							 iconUrl: 'assets/download2.png',
-							 //shadowUrl: 'assets/download.png'		
-					})
-					
-					
-						
-				});
-				this.zone.run(() => {
-
-          let location1: GeoCoordinate = {
-              latitude: 8.879966,
-              longitude: 38.726909
-            };
-
-
-					this.lat = e.latlng.lat;
-					this.lang = e.latlng.lng;
-
-          let location2: GeoCoordinate = {
-            latitude: this.lat,
-            longitude: this.lang
-          };
-
-          this.distance = this.geoDistanceService.getDistanceInKilometers(location1, location2);
-
-				});
-				c.addTo(map);
-				});
 	
-				
-				
-		}
 
-}
+//onMapReady(map: L.Map) {
+	//map.on('click',
+		//<LeafletMouseEvent>(e) => {
+
+			//let c = L.marker([e.latlng.lat, e.latlng.lng], {
+				//icon: L.icon({
+					//iconSize: [25, 41],
+					//iconAnchor: [13, 41],
+
+				///	iconUrl: 'assets/download2.png',
+					//shadowUrl: 'assets/download.png'		
+			//	})
+
+
+
+		//	});
+	//		this.zone.run(() => {
+
+		//		let location1: GeoCoordinate = {
+			//		latitude: 8.879966,
+				//	longitude: 38.726909
+			//	};
+
+
+		//		this.lat = e.latlng.lat;
+		//		this.lang = e.latlng.lng;
+
+			//	let location2: GeoCoordinate = {
+				//	latitude: this.lat,
+			//		longitude: this.lang
+			//	};
+
+			//	this.distance = this.geoDistanceService.getDistanceInKilometers(location1, location2);
+
+			//});
+			//c.addTo(map);
+		//});
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Ng2CarouselamosModule } from 'ng2-carouselamos';
 
 
 import { AppComponent } from './app.component';
@@ -8,12 +9,15 @@ import { AppComponent } from './app.component';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { GeoDistanceService } from './geo-distance.service';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule, LeafletModule
+    BrowserModule,
+     LeafletModule,
+     Ng2CarouselamosModule
   ],
   providers: [GeoDistanceService],
   bootstrap: [AppComponent]
