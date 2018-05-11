8437c78d7e4a78b803e46de1e2f15e07a73d5ddc
honey <honey@localhost.localdomain>
2018-05-10 22:59:44 -0500

    the images are controlled by a button,icon changed,added lon and lat to each pic

--- src/app/addis-images/addis-images.component.ts
+++ src/app/addis-images/addis-images.component.ts
@@ -1,19 +1,49 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map } from 'rxjs/operators';import { Subject, Observable } from 'rxjs';
+import { PHOTOS } from './pic'
 
 
 @Component({
   selector: 'app-addis-images',
   templateUrl: './addis-images.component.html',
+  template: `
+  <img src="{{photos[(counter$ | async)]}}">
+
+  <button (click)="button$.next(-1)">Prev</button>
+  <button (click)="button$.next(1)">Next</button>
+
+  
+  <button (click)="button$.next(1)">submit</button>
+`,
  
 })
 export class AddisImagesComponent implements OnInit {
 
   images: Array<string>;
 
-  constructor(private _http: HttpClient) { }
+ 
+   photos = PHOTOS;
+   START = 0;
+   TOTAL = PHOTOS.length;
+   button$ = new Subject();
+   counter$: Observable<any>;
+   constructor(private _http: HttpClient) {
+     this.counter$ = Observable.merge(
+       this.button$,
+       Observable.interval(5000).mapTo(1)
+     )
+       .startWith(this.START)
+       .scan((acc: number, curr: any) => {
+         // if next && last image
+         if (curr === 1 && acc === this.TOTAL - 1) return 0;
+         // if prev && first image
+         if (curr === -1 && acc === 0) return this.TOTAL - 1;
+         // all other cases
+         return acc + curr;
+       })
+     }
 
   ngOnInit() {
     this._http.get('http://localhost:4200/assets/Addis-Pictures.json')
@@ -28,5 +58,6 @@ export class AddisImagesComponent implements OnInit {
       return `http://localhost:4200/assets/images/${randomId}.jpg`;
     });
   }
+  
   
 }
---
+++ src/app/addis-images/pic.ts
@@ -0,0 +1,10 @@
+export const PHOTOS = [
+    'http://localhost:4200/assets/images/1.jpg',
+    'http://localhost:4200/assets/images/2.jpg',
+    'http://localhost:4200/assets/images/3.jpg',
+    'http://localhost:4200/assets/images/4.jpg',
+    'http://localhost:4200/assets/images/5.jpg',
+    'http://localhost:4200/assets/images/6.jpg',
+    'http://localhost:4200/assets/images/7.jpg',
+    'http://localhost:4200/assets/images/8.jpg'
+];
