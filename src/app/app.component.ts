a202dc7540d42fd3488db87a0a53beed66a3e59c
honey <honey@localhost.localdomain>
2018-08-13 14:40:53 -0500

    new_component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+	selector: 'app-root', 
+	template: ` <div styl="padding:5px">
+		   <ul class="nav nav-tabs">
+			<li> <a routerLink="home"> Home </a> </li>
+			<li> <a routerLink="addis-map"> addis-map </a> </li>
+			</ul>
+			<router-outlet></router-outlet>
+			</div>`
 })
+
 export class AppComponent {
 	
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 
 // other imports...
@@ -9,25 +9,41 @@ import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { GeoDistanceService } from './addis-map/geo-distance.service';
 import { AddisImageService } from './addis-images/addis-image.service';
 import { AddisMapComponent } from './addis-map/addis-map.component';
-
-
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+const appRoutes: Routes =[
+{ path: 'home', component: HomeComponent },
+{ path: 'addis-map', component: AddisMapComponent },
+{ path: '', redirectTo: '/home',pathMatch: 'full' },
+{ path: '**', component: ErrorComponent }
+];
+
 @NgModule({
   declarations: [
+    
     AppComponent,
-    AddisMapComponent
+    AddisMapComponent,
+    HomeComponent,
+    ErrorComponent,
+ 
+
   ],
   imports: [
+    CommonModule,
     BrowserModule, 
     LeafletModule, 
     FormsModule, 
     ReactiveFormsModule, 
     HttpClientModule, 
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
+
   ],
+  
   providers: [GeoDistanceService, AddisImageService],
   bootstrap: [AppComponent]
 })
